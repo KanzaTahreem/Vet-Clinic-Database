c2a24749184f44ae016618a834e117a971b76e5e
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-12-08 01:13:20 +0500

    Fix schema with correct SQL used for table

--- schema.sql
+++ schema.sql
@@ -1,13 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-  id INT GENERATED ALWAYS AS IDENTITY,
+  id SERIAL PRIMARY KEY,
   name VARCHAR(250),
   date_of_birth DATE,
   escape_attempts INT,
   neutered BOOLEAN,
-  weight_kg DECIMAL,
-  PRIMARY KEY(id)
+  weight_kg DECIMAL
 );
 
 ALTER TABLE animals ADD species VARCHAR(250);
