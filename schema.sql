1c7e1446ebbb965a26d28a8b0d4f56305e06d39a
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-12-08 02:27:50 +0500

    Alter animals table

--- schema.sql
+++ schema.sql
@@ -21,3 +21,9 @@ CREATE TABLE species (
   id SERIAL PRIMARY KEY,
   name VARCHAR(250)
 );
+
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD species_id INT;
+ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species(id);
+ALTER TABLE animals ADD owner_id INT;
+ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
