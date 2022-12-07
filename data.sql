78261c68283dcb6a4fad1483b010d95800b8d1cd
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-12-08 02:30:08 +0500

    Insert data into the owners table

--- data.sql
+++ data.sql
@@ -14,3 +14,11 @@ VALUES (5, 'Charmander', '2020-02-08', 0, false, -11),
        (9, 'Boarmon', '2005-06-07', 7, true, 20.4),
        (10, 'Blossom', '1998-10-13', 3, true, 17),
        (11, 'Ditto', '2022-05-14', 4, true, 22);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Sam Smith', 34),
+       ('Jennifer Orwell', 19),
+       ('Bob', 45),
+       ('Melody Pond', 77),
+       ('Dean Winchester', 14),
+       ('Jodie Whittaker', 38);
