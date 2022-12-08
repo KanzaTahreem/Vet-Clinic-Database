8baa15b08d7ee7033d71585cc72fd5ffc5001031
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-12-09 01:04:58 +0500

    Create vets table

--- schema.sql
+++ schema.sql
@@ -27,3 +27,10 @@ ALTER TABLE animals ADD species_id INT;
 ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species(id);
 ALTER TABLE animals ADD owner_id INT;
 ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+CREATE TABLE vets (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(250),
+  age INT,
+  date_of_graduation DATE
+);
