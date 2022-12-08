eb975dcfa9f4d856446f5796d49c757b08eb543a
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-12-09 01:12:49 +0500

    Insert data into the `specializations` table

--- data.sql
+++ data.sql
@@ -42,3 +42,10 @@ VALUES ('William Tatcher', 45, '2000-04-23'),
        ('Stephanie Mendez', 64, '1981-05-04'),
        ('Jack Harkness', 38, '2008-06-08');
 
+
+INSERT INTO specializations (vets_id, species_id)
+VALUES ((SELECT id FROM vets WHERE name = 'William Tatcher'), (SELECT id FROM species WHERE name = 'Pokemon')),
+       ((SELECT id FROM vets WHERE name = 'Stephanie Mendez'), (SELECT id FROM species WHERE name = 'Digimon')),
+       ((SELECT id FROM vets WHERE name = 'Stephanie Mendez'), (SELECT id FROM species WHERE name = 'Pokemon')),
+       ((SELECT id FROM vets WHERE name = 'Jack Harkness'), (SELECT id FROM species WHERE name = 'Digimon'));
+
