7c257ba0ac138164addbf0128860b05fb3d0538c
KanzaTahreem <tehreemmsk411@gmail.com>
2022-12-14 02:12:21 +0500

    Add realtionships between tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,7 +1,7 @@
 CREATE TABLE patients (
   id SERIAL PRIMARY KEY,
   name VARCHAR(250),
-  date_of_birth DATE,
+  date_of_birth DATE
 );
 
 CREATE TABLE invoices (
@@ -9,14 +9,14 @@ CREATE TABLE invoices (
   total_amount DECIMAL,
   generated_at TIMESTAMP,
   payed_at TIMESTAMP,
-  medical_history_id INT,
+  medical_history_id INT FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
 );
 
 CREATE TABLE medical_histories (
   id SERIAL PRIMARY KEY,
   admitted_at TIMESTAMP,
-  patient_id INT,
-  status VARCHAR(250),
+  patient_id INT FOREIGN KEY (patient_id) REFERENCES patients(id),
+  status VARCHAR(250)
 );
 
 CREATE TABLE treatments (
@@ -30,8 +30,8 @@ CREATE TABLE invoice_items (
   unit_price DECIMAL,
   quantity INT,
   total_price DECIMAL,
-  invoice_id INT,
-  treatment_id INT
+  invoice_id INT FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+  treatment_id INT FOREIGN KEY (treatment_id) REFERENCES treatments(id)
 );
 
 CREATE TABLE treatment_history (
